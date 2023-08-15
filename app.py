fdf504cebee633104775f2ee46b6eb6bf12b2583
pankaj <pankajraj7651@gmail.com>
2023-08-15 13:12:23 +0530

    app

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "CI CD pipeline has been establishesd."
+    return "CI CD pipeline has been establishesd. "
 
 if __name__=="__main__":
     app.run(debug=True)  
