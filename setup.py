ec8919addec126a4823fe68f531f7f883d3961af
pankaj <pankajraj7651@gmail.com>
2023-08-25 09:15:28 +0530

    setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,26 @@ from setuptools import setup
 from typing import List
 
 
-def get_requirements_list()->List:
-    pass
+
 #Declaring variables for setup functions 
 PROJECT_NAME="housing-predictor"
 VERSION="0.0.1"
 AUTHOR="Panakj Raj"
 DESRCIPTION="This is a first FSDS Nov batch Machine Learning Project"
 PACKAGES=["housing"]
+REQUIREMENT_FILE_NAME="requirements.txt"
+
+
+def get_requirements_list()->List[str]:
+    """
+    Description: This function is going to return list of requirement
+    mention in requirements.txt file
+
+    return This function is going to return a list which contain name 
+    of lisbraries mentioned in requirements.txt file
+    """
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        return requirement_file.readline()
 
 setup(
     name=PROJECT_NAME,
@@ -21,3 +33,4 @@ setup(
 
 
 )
+
